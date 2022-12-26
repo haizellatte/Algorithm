1ee37f9c9aeb44d2a0592a33bc90e5ac67e715ad
haizel <115685195+haizellatte@users.noreply.github.com>
2022-12-26 10:21:39 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
+++ 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
@@ -1,9 +1,15 @@
 function solution(array, height) {
-let count = 0;
-for(let el of array){
-    if(el > height){
-        count++;
-    }
+let result = array.filter(el => el > height)
+return result.length;
 }
-    return count;
-}
+
+
+// function solution(array, height) {
+// let count = 0;
+// for(let el of array){
+//     if(el > height){
+//         count++;
+//     }
+// }
+//     return count;
+// }
