5baf61ef9b28103b3f24dddb9a4ea3505bf0a264
haizel <115685195+haizellatte@users.noreply.github.com>
2023-03-06 22:14:02 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.js
+++ 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.js
@@ -1,9 +1,4 @@
 function solution(my_string) {
-let result = '';
-    for(let i of my_string){
-        if(i !== ' '){
-            result += i
-        }
-    }
-    return eval(result);
+
+    return eval(my_string);
 }
