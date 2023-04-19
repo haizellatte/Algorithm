6b46301e4cd8f4de6077f57e92f6685722aeb44e
haizel <115685195+haizellatte@users.noreply.github.com>
2023-04-19 09:32:33 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.js
+++ 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.js
@@ -3,9 +3,11 @@ function solution(chicken) {
     let serviceChicken = 0;
     while (coupon >= 10) {
         // serviceChicken += Math.floor(coupon / 10);
-        serviceChicken += (coupon / 10) << 0;
+        // serviceChicken += (coupon / 10) << 0;
+        serviceChicken += parseInt(coupon / 10);
         // coupon = coupon % 10 + coupon / 10;
-        coupon = ((coupon % 10) + coupon / 10) << 0;
+        // coupon = ((coupon % 10) + coupon / 10) << 0;
+        coupon = (coupon % 10) + parseInt(coupon / 10);
     }
     return serviceChicken;
 }
