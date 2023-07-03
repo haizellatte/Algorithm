47077a26b93a2cf1d2bdf346678ee97e47c8238a
haizel <115685195+haizellatte@users.noreply.github.com>
2023-07-03 16:57:10 +0900

    Update 단어 정렬.js

--- 백준/Silver/1181. 단어 정렬/단어 정렬.js
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.js
@@ -3,14 +3,14 @@ let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
 const iter = input.shift();
 
-// 중복되는 값을 제거하는 Set 객체(new Set)
-// Set 객체를 배열 형태로 전환(Array.from)
-let arr = Array.from(new Set(input));
+// 중복되는 값을 제거하는 Set 객체(new Set)를 사용해 집합으로 변환한 후, 
+//--> 배열 형태로 전환(rest 또는 Array.from)
+let arr = [...new Set(input)];
 
-// 사전 순서로 정렬
+// 2순위) 사전 순서로 정렬
 arr.sort((a, b) => a > b ? 1 : (a < b ? -1 : 0));
 
-// 길이 순서로 정렬
+//1순위) 길이 순서로 정렬
 arr.sort((a,b) => a.length - b.length);
 
 console.log(arr.join("\n"));
