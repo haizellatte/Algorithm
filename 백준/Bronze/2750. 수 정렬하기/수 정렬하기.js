04ebc5565c89754dbc54fd8f35358227e68e1b33
haizel <115685195+haizellatte@users.noreply.github.com>
2023-07-03 15:31:32 +0900

    Update 수 정렬하기.js

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.js
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 // 엔터로 구분되어 있는 값들을 split으로 처리하고, map을 사용해서 number 타입으로 변환
 const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n").map((item) => Number(item));
 
-// 가장 앞에 있는 값은 배열 길이를 의미
+// 첫째 줄의 수는 배열의 길이를 의미하기 때문에 shift로 제거해준다.
 const iter = input.shift();
 
 // 오름차순 정렬 수행
