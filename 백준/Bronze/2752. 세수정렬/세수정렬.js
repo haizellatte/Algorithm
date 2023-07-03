052bb480e5231fe6b8bfdf8a95eabe6e371e9b88
haizel <115685195+haizellatte@users.noreply.github.com>
2023-07-03 15:23:32 +0900

    Update 세수정렬.js

--- 백준/Bronze/2752. 세수정렬/세수정렬.js
+++ 백준/Bronze/2752. 세수정렬/세수정렬.js
@@ -6,7 +6,7 @@ let arr = input[0].split(' ').map(Number);
 // 오름차순 정렬 수행
 arr.sort((a, b) => a - b);
 
-// 정답 리턴
+// 한번에 콘솔 호출로 답을 리턴하기 위해 문자열로 정답을 만든다.
 let answer = "";
 for (let el of arr) {
     answer += el + " "
