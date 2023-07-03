90bc209c30c055513aef62f4cc8bd59ba4390e01
haizel <115685195+haizellatte@users.noreply.github.com>
2023-07-03 15:52:57 +0900

    Update 수 정렬하기 2.js

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.js
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const input = fs.readFileSync("dev/stdin").toString().trim().split("\n").map((el) => Number(el));
 
 // 첫째 줄의 수는 배열의 길이를 의미하기 때문에 shift로 제거해준다.
-const iter = input.shift();
+input.shift();
 // 오름차순 정렬 수행
 input.sort((a, b) => a - b);
 
