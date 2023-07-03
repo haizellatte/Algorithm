285c57bef2dfeb434bf87e387f0c0d63bc99f731
haizel <115685195+haizellatte@users.noreply.github.com>
2023-07-03 15:43:28 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1264 ms, Memory: 217228 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const input = fs.readFileSync("dev/stdin").toString().trim().split("\n").map((el) => Number(el));
+
+// 첫째 줄의 수는 배열의 길이를 의미하기 때문에 shift로 제거해준다.
+const iter = input.shift();
+// 오름차순 정렬 수행
+input.sort((a, b) => a - b);
+
+/* 기존의 for문으로 출력하면 시간 초과로 탈락, 
+   join을 통해 하나의 문자열로 만들어 한번에 출력한다. */
+console.log(input.join("\n"));
