57d74698ec38aaf86e15aca5af12e2b6d81fd710
haizel <115685195+haizellatte@users.noreply.github.com>
2023-07-06 20:39:38 +0900

    [unrated] Title: 공백으로 구분하기 2, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.js
+++ 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.js
@@ -1,11 +1,34 @@
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
 function solution(my_string) {
-  let str = my_string.split(" ")
-  let arr = [];
+    let answer = [""];
 
-  for (let i = 0; i < str.length; i++) {
-    if (str[i] !== "" ) {
-      arr.push(str[i])
+    for(let i=0;i<my_string.length;i++) {
+        if(my_string[i] === " ") {
+            if(answer[answer.length - 1] !== "")
+                answer.push("");
+        } else {
+            answer[answer.length - 1] += my_string[i];
+        }
     }
-  }
-  return arr;
+
+    if(answer[answer.length - 1] === "")
+        answer.pop();
+
+    return answer;
 }
