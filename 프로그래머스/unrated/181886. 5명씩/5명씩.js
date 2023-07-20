4ff0f2c72dd320b8444c5c39d1539b89d467bb8a
haizel <115685195+haizellatte@users.noreply.github.com>
2023-07-20 15:26:42 +0900

    [unrated] Title: 5명씩, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181886. 5명씩/5명씩.js
+++ 프로그래머스/unrated/181886. 5명씩/5명씩.js
@@ -1,8 +1,3 @@
 function solution(names) {
-    let answer = [];
-    
-    for (let i = 0; i < names.length; i += 5) {
-        answer.push(names[i])
-    }
-    return answer;
+  return names.filter((_, i) => i % 5 === 0);
 }
