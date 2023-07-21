efcf89cc4674dfec6e8e238d7a48b9435fd82072
haizel <115685195+haizellatte@users.noreply.github.com>
2023-07-21 14:00:57 +0900

    [unrated] Title: 두 수의 연산값 비교하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.js
@@ -0,0 +1,16 @@
+function stickNum(a, b) {
+    let num = String(a) + String(b);
+    return Number(num);
+}
+
+function twoByNum(a, b) {
+    return 2 * a * b;
+}
+
+function solution(a, b) {
+ if (stickNum(a, b) === twoByNum(a, b)) {
+     return stickNum(a, b);
+ } else {
+     return stickNum(a, b) > twoByNum(a, b) ? stickNum(a, b) : twoByNum(a, b);
+ }
+}
