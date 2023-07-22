05ba752019f6fe57b5a73609b60016255fd0934c
haizel <115685195+haizellatte@users.noreply.github.com>
2023-07-22 14:37:30 +0900

    [unrated] Title: 글자 지우기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.js
+++ 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.js
@@ -1,13 +1,3 @@
 function solution(my_string, indices) {
-    let arr = [...my_string];
-    let result = [];
-    
-    for (let i = 0; i < arr.length; i++) {
-        if (indices.includes(i)) {
-            continue;
-        } else {
-            result.push(arr[i]);
-        }
-    }
-    return result.join("");
+  return [...my_string].filter((_, i) => !indices.includes(i)).join("")
 }
