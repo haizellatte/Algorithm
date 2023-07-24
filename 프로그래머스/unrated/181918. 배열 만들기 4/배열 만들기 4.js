39e9c36405f596829931d7b9aedb974c655204ee
haizel <115685195+haizellatte@users.noreply.github.com>
2023-07-24 17:48:58 +0900

    [unrated] Title: 배열 만들기 4, Time: 2.93 ms, Memory: 37.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.js
@@ -0,0 +1,28 @@
+/*
+* [풀이 노트]
+* 0.변수 i를 초기값 0으로 설정하고, i < arr.length일 때 아래 작업을 반복한다.
+* 1. stk.length === 0 ? stk.push(arr[i]) i++;
+* 2. stk.length > 0 && stk[stk.length - 1] < arr[i] ? stk.push(arr[i]) i++;
+* 3. stk.length > 0 && stk[stk.length - 1] >= arr[i] ? stk.pop();
+* 4. return stk;
+*/
+
+
+function solution(arr) {
+    let stk = [];
+    let i = 0;
+    while(i < arr.length) {
+        if (stk.length === 0) {
+            stk.push(arr[i]);
+            i++;
+        } else {
+            if (stk[stk.length - 1] < arr[i]) {
+                stk.push(arr[i]);
+                i++;
+            } else {
+                stk.pop();
+            }
+        }
+    }
+        return stk;
+}
