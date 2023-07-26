0b2ea7d3dbeb794d74a687b0293ef278057d9dbe
haizel <115685195+haizellatte@users.noreply.github.com>
2023-07-26 15:11:33 +0900

    [unrated] Title: 부분 문자열, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181842. 부분 문자열/부분 문자열.js
+++ 프로그래머스/unrated/181842. 부분 문자열/부분 문자열.js
@@ -1,3 +1,12 @@
 function solution(str1, str2) {
-    return str2.includes(str1) ? 1 : 0;
+ // for (let i = 0; i <= str2.length; i++) {
+ //     for (let j = i+1; j <= str2.length; j++) {
+ //         if (str2.slice(i, j) === str1) {
+ //             return 1;
+ //         }
+ //         return 0;
+ //     }
+ // }
+    
+    return str2.slice(1,4) === str1 ? 1: 0
 }
