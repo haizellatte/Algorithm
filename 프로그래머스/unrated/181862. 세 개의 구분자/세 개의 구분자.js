755f89d6e71022c4f8c496e9cfdc959cfcda9741
haizel <115685195+haizellatte@users.noreply.github.com>
2023-07-26 16:44:03 +0900

    [unrated] Title: 세 개의 구분자, Time: 28.95 ms, Memory: 57.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.js
+++ 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.js
@@ -1,4 +1,11 @@
-function solution(myStr) {
-  const answer = myStr.split(/[a|b|c]/).filter(x => x);
-  return answer.length ? answer : ["EMPTY"];
+function splitWord(s, a, b) {
+  return b ? s.split(a).join(b) : s.split(a).filter(x => x);
+}
+
+function solution(s) {
+  const splitA = splitWord(s, 'a', 'b');
+  const splitB = splitWord(splitA, 'b', 'c');
+  const splitC = splitWord(splitB, 'c');
+    
+  return splitC.length > 0 ? splitC : ["EMPTY"];
 }
