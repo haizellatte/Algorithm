b981c0e4303db851e860243e1aa32650e3395fe0
haizel <115685195+haizellatte@users.noreply.github.com>
2023-07-28 11:05:42 +0900

    [unrated] Title: 수열과 구간 쿼리 1, Time: 2.54 ms, Memory: 35.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.js
@@ -0,0 +1,10 @@
+// 문제 : queries의 각 배열을 query라고 할때, arr[query]에 해당하는 값에 +1을 더한 배열을 리턴하세요.
+
+function solution(arr, queries) {
+    queries.map(([s, e]) => {
+        for (let i = s; i <= e; i++) {
+            arr[i] += 1;
+        } 
+    })
+   return arr; 
+}
