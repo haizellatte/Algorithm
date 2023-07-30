acc4eabb93abdf093b5781fad1b18b474111ce06
haizel <115685195+haizellatte@users.noreply.github.com>
2023-07-30 17:39:59 +0900

    [unrated] Title: 수열과 구간 쿼리 2, Time: 10.12 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
+++ 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
@@ -1,13 +1,13 @@
-
-
 function f(arr, query) {
-    const [s, e, k] = query;
-    const filtered = arr.filter((v, i) => s <= i && i <= e && v > k);
-    
-    return filtered.length ? Math.min(...filtered) : -1;
+  const [s, e, k] = query;
+  const filtered = arr
+    .slice(s, e + 1)
+    .filter((x) => x > k)
+    .sort((a, b) => a - b);
+
+  return filtered[0] !== undefined ? filtered[0] : -1;
 }
 
-
 function solution(arr, queries) {
-    return queries.map((query) => f(arr, query));
+  return queries.map((query) => f(arr, query));
 }
