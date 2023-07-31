2f9a42ab4802dd927a4a3889c982b052aec3b694
haizel <115685195+haizellatte@users.noreply.github.com>
2023-07-31 15:58:56 +0900

    [unrated] Title: 수열과 구간 쿼리 4, Time: 8.38 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.js
+++ 프로그래머스/unrated/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.js
@@ -1,21 +1,12 @@
-
-// 조건 1 확인
-function checkI(s, e, i) {
-  return (s <= i && i <= e);
-}
-
-// 조건 2 확인
-function checkMultiple(i, k) {
-  return (i % k === 0);
-}
-
 function solution(arr, queries) {
-    arr.map((v, i) => {
-      queries.map(([s, e, k]) => {
-        if (checkI(s, e, i) && checkMultiple(i, k)) {
-          arr[i] += 1;
-        }
-      });
-    })
-  return arr;
+    return queries.reduce((acc, cur) => {
+        const copyArr = [...acc]
+        const [to, from, val] = cur
+        return copyArr.map((el, elIdx) => {
+            if(elIdx >= to && elIdx <= from) {
+                if(elIdx%val === 0) return el+1
+            }
+            return el
+        })
+    }, arr)
 }
