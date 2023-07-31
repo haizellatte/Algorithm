e37737f9c5dc51ed26f5a4c412709f24fefa9e4b
haizel <115685195+haizellatte@users.noreply.github.com>
2023-07-31 15:25:17 +0900

    [unrated] Title: 무작위로 K개의 수 뽑기, Time: 2.56 ms, Memory: 35.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.js
+++ 프로그래머스/unrated/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.js
@@ -1,9 +1,8 @@
-function solution(arr, k) {
-    // 1. k 개수만큼 -1로 채운 배열을 만든다.
-    const result = Array(k).fill(-1);
-    // 2. 중복되지 않은 원소들을 구한다.
-    const onlyNum = arr.filter((v, i) => arr.indexOf(v) === i);
-    // result 배열의 0 ~ onlyNum.length 만큼을 2번에서 구한 배열의 값으로 바꾼다.
-    result.splice(0, onlyNum.length, ...onlyNum);
-    return result.slice(0, k);
+const solution = (arr, k) => {
+    const set = new Set(arr);
+    const result = [...set].slice(0,k);
+    while(result.length !== k){
+        result.push(-1);
+    }
+    return result;
 }
