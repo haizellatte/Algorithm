d885a38bd8587b3098672753f75fa9f99c82eafb
haizel <115685195+haizellatte@users.noreply.github.com>
2023-08-01 17:48:55 +0900

    [unrated] Title: 그림 확대, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181836. 그림 확대/그림 확대.js
+++ 프로그래머스/unrated/181836. 그림 확대/그림 확대.js
@@ -1,34 +1,16 @@
-// 1x1 크기의 정사각형으로 이루어진 배열 picture가 주어질 때, 가로 세로로 k배 늘린 배열을 리턴하세요.
-/*
-* [아이디어 노트]
-* 1. arr[i]의 각 요소 x을 k만큼 반복한다.
-* 2. 2번에서 완성한 문자열을 k만큼 반복한다.
-*/
-
-function repeat(x, k) {
-    const bucket = [];
-    for (let i = 0; i < k; i++) {
-        bucket.push(x);
-    }
-    return bucket;
-}
-
-
 function solution(picture, k) {
-    const expansionPic = [];
+  let expansionPic = [];
 
-   // 1. arr[i]의 각 요소를 k만큼 반복한다.     
-    for (let i = 0 ; i < picture.length; i++) {
-        let str = '';
-        // picture[i][j] 값을 k만큼 반복
-        for (let j = 0; j < picture[i].length; j++) {
-            for (let n = 0 ; n < k; n++) {
-                str+= picture[i][j];
-            }
-        }
-        expansionPic.push(str);   
+  // 1. picture의 각 문자열에 map을 돌려 반복한다.
+  picture.map(x => {
+    // 2. picture[i]의 각 자리수를 배열로 만든 후 돌면서(map), 각 값을 k만큼 반복한 후 문자열로 변환하여 변수에 담는다.
+    const bucket = [...x].map(v => v.repeat(k)).join('');
+    // 3. 2번에서 만든 배열을 k번만큼 반복해 리턴할 배열에 담는다.
+    for (let i = 0; i < k; i++) {
+      expansionPic.push(bucket);
     }
 
-     // 2. 2번에서 완성한 문자열을 k만큼 반복한다.
-     return expansionPic.map(x => repeat(x, k, expansionPic)).flat()
+  })
+
+    return expansionPic;
 }
