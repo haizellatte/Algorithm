09b8bd13de90d7340c8bfb27dd995a68e62f39f0
haizel <115685195+haizellatte@users.noreply.github.com>
2023-08-01 15:59:24 +0900

    [unrated] Title: 배열 만들기 6, Time: 3.55 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.js
+++ 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.js
@@ -12,18 +12,15 @@ function solution(arr) {
     let i = 0;
     
     while(i < arr.length) {
-        if (stk.length > 0) {
-            if (stk[stk.length - 1] === arr[i]) {
-                stk.pop(); i++;
-            }
-            else if(stk[stk.length - 1] !== arr[i]) {
-                stk.push(arr[i]); i++;
-            }
+        if (stk.length > 0 && stk[stk.length - 1] === arr[i]) {
+             stk.pop(); i++;
         }
-        else if (stk.length === 0){
-            stk.push(arr[i]); i++;
+       else if (stk.length >= 0 || stk[stk.length - 1] !== arr[i]) {
+           stk.push(arr[i]); i++;
+       }
+             
         }
-    }
+    
     
     return stk.length !== 0 ? stk : [-1];
     
