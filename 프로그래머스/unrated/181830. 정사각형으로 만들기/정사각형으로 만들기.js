94dda5d3f4b26414160c0cbac7d9bbfa199fad2b
haizel <115685195+haizellatte@users.noreply.github.com>
2023-08-01 16:38:37 +0900

    [unrated] Title: 정사각형으로 만들기, Time: 0.08 ms, Memory: 38.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181830. 정사각형으로 만들기/정사각형으로 만들기.js
@@ -0,0 +1,27 @@
+// 문제 : 2차원 배열 arr에 대해 1) 행과 열의 수가 같아지도록 각 행과 열에 끝에 0을 추가한 이차원 배열을 리턴하세요.
+
+function solution(arr) {
+    let rows = arr.length;
+    let columns = arr[0].length;
+    
+    // 행 > 열     
+    if (rows > columns) {
+        // arr[i]에 대해
+        for (let i = 0 ; i < arr.length; i++) {
+            // 행-열의 차이만큼 반복           
+            for (let j = columns; j < rows; j++) {
+                arr[i].push(0);
+            }
+        }
+    } 
+    
+    // 행 < 열
+    if (rows < columns) {
+        for (let i = rows; i < columns; i++) {
+            arr.push(Array(columns).fill(0));
+        }
+    }
+        
+    
+    return arr;
+}
