545563e991abb64e2b4d45eeadbe0e78a57b4c6c
haizel <115685195+haizellatte@users.noreply.github.com>
2023-08-09 15:11:04 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,7 +1,5 @@
+
 function solution(phone_number) {
-    const e = phone_number.length;
-    const lastN = phone_number.substring(e-4, e);
-    const ChangeN = [...phone_number.substring(0, e-4)].map(v => "*").join('')
-   
-   return ChangeN.concat(lastN);                                       
+  const e = phone_number.length - 4;
+  return [...phone_number].fill("*", 0, e).join("")      
 }
