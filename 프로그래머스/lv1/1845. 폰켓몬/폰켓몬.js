98cbec4fc16ba5a70db4bbc2788e24ee01967455
haizel <115685195+haizellatte@users.noreply.github.com>
2023-08-20 10:26:13 +0900

    [level 1] Title: 폰켓몬, Time: 0.27 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
@@ -1,6 +1,6 @@
 function solution(nums) {
-    const max = nums.length /2;
-    const arr = nums.filter((x, i) => nums.indexOf(x) === i).length;
-    
-    return max < arr ? max : arr;
+  const max = nums.length / 2;
+  const arr = [...new Set(nums)].length;
+
+  return max < arr ? max : arr;
 }
