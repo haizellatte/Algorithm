c139e78f2b4e65fae3537a78388aadb91d41e26b
haizel <115685195+haizellatte@users.noreply.github.com>
2023-08-29 21:07:30 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,16 +1,30 @@
 function solution(s) {
-    let answer = s;
-    let numbers = ['zero','one','two','three','four','five','six','seven','eight','nine'];
-    
-    /*
-    1. 반복문을 돌면서 숫자를 기준으로 문자열을 잘라준다.
-    2. 자른 단어가 numbers 배열과 일치하면 해당 index를 반환하여 출력
-    3. 문자열로 잘리는게 없다면 그대로 둔다.
-    */
-    
-    for(let i = 0; i < numbers.length; i++) {
-        let newArr = answer.split(numbers[i]);
-        answer = newArr.join(i)
+  const alphabet = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+  ];
+
+  let result = "";
+  let temp = "";
+  for (let i = 0; i < s.length; i++) {
+    if (isNaN(s[i])) { // 문자인 경우
+      temp += s[i];
+      if (alphabet.includes(temp)) {
+        result += alphabet.indexOf(temp);
+        temp = "";
+      }
+    } else { // 숫자인 경우
+      result += s[i];
     }
-    return Number(answer);
+  }
+
+  return Number(result);
 }
