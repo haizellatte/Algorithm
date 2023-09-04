0cf3e9612a3d72fbe16552662759b02043df560f
haizel <115685195+haizellatte@users.noreply.github.com>
2023-09-04 16:33:02 +0900

    [level 0] Title: 옹알이 (1), Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.js
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -1,5 +1,15 @@
 function solution(babbling) {
-  return babbling
-    .map((v) => v.replace(/(aya|ye|woo|ma)/g, ""))
-    .filter((v) => !v).length;
+  const able = ["aya", "ye", "woo", "ma"];
+  let count = 0;
+
+  for (let b of babbling) {
+    for (let a of able) {
+      b = b.replace(a, ' ');
+    }
+    if (b.trim() === '') { // 양쪽 공백 제거 --> 요소가 2개 이상 있을 수도 있으니까
+      count++;
+    }
+  }
+
+  return count;
 }
