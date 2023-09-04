c179e9e1ce60ec509a571cfb9c82cf382499d699
haizel <115685195+haizellatte@users.noreply.github.com>
2023-09-04 13:14:06 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
@@ -2,14 +2,12 @@ const pointCalculator = {
   S: (v) => Math.pow(v, 1),
   D: (v) => Math.pow(v, 2),
   T: (v) => Math.pow(v, 3),
-  "*": (pre, cur) => pre * 2 + cur * 2,
-  "#": (pre, cur) => cur * (-1),
 };
 
 function solution(dartResult) {
   //* 1. 요소가 숫자이고 (현재 요소 + 앞 요소)가 10이 아닌 경우(점수가 10까지 있기 때문에) 요소 앞에 공백을 추가하고 공백으로 split 한 후 1번째 요소부터 가져온다. (맨 앞 숫자의 공백을 제외하기 위해)
   const [a, b, c] = [...dartResult]
-    .map((x, i) => (!isNaN(x) && dartResult[i - 1] + x !== "10" ? " " + x : x))
+    .map((x, i) => (!isNaN(x) && (dartResult[i - 1] + x) !== "10" ? " " + x : x))
     .join("")
     .split(" ")
     .slice(1);
@@ -17,7 +15,6 @@ function solution(dartResult) {
   const bonus = ['S', 'D', 'T'];
   const option = ['*', '#'];
   let bonusPoint = [];
-  let result = 0;
     
   //* 2. bonus 점수 확인
   for (let point of [a, b, c]) {
@@ -25,7 +22,7 @@ function solution(dartResult) {
     let bonusValue = '';
     
     for (let i = 0; i < point.length; i++) {
-      if (!isNaN(point[i])) {
+      if (!isNaN(point[i])) { // 숫자라면
         pointValue += point[i];
       }
       if (bonus.includes(point[i])) {
@@ -38,7 +35,6 @@ function solution(dartResult) {
     }
     bonusPoint.push([pointValue, bonusValue]);
   }
-  console.log(bonusPoint)
 
   //* 3. option 점수 확인
   for (let i = 0; i < bonusPoint.length; i++) {
@@ -48,6 +44,7 @@ function solution(dartResult) {
       }
       if (bonusPoint[i][1] === "*") {
         bonusPoint[i][0] *= 2; 
+
         if (i - 1 >= 0) {
           bonusPoint[i - 1][0] *= 2;
         }
