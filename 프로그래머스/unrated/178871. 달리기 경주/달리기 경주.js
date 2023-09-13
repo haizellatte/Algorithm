81232e35753a134dce407436bc8e4905b2d87c6a
haizel <115685195+haizellatte@users.noreply.github.com>
2023-09-13 16:05:18 +0900

    [unrated] Title: 달리기 경주, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.js
@@ -0,0 +1,21 @@
+function MakeObject(players, keyOption){
+  return players.reduce((acc, cur, i) => {
+    keyOption ? (acc[cur] = i + 1) : (acc[i+1] = cur);
+    return acc;
+  }, {});
+}
+
+function solution(players, callings) {
+  const keyPlayers = MakeObject(players, true); //* 선수를 key로 갖는 객체 --> { mumu: 1, soe: 2, poe: 3, kai: 4, mine: 5 }
+  const keyRanks = MakeObject(players, false); //* 현재 등수를 key로 갖는 객체 --> { '1': 'mumu', '2': 'soe', '3': 'poe', '4': 'kai', '5': 'mine' }
+
+  for (let i = 0; i < callings.length; i++) {
+    const beforeCallingPlayer = keyRanks[keyPlayers[callings[i]] - 1];
+    keyRanks[keyPlayers[callings[i]]] = beforeCallingPlayer;
+    keyRanks[keyPlayers[beforeCallingPlayer]] = callings[i];
+    keyPlayers[callings[i]] -= 1;
+    keyPlayers[beforeCallingPlayer] += 1;
+  }
+
+  return Object.values(keyRanks);
+}
