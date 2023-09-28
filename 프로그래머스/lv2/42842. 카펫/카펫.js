0a5e6ad40db4ade60d60f6e73967889d63e2cead
haizel <115685195+haizellatte@users.noreply.github.com>
2023-09-28 13:15:38 +0900

    [level 2] Title: 카펫, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.js
+++ 프로그래머스/lv2/42842. 카펫/카펫.js
@@ -3,13 +3,11 @@ function solution(brown, yellow) {
   let [x, y] = [1, 1];
 
   for (let i = 1; i <= Math.sqrt(total); i++) {
-    if (total % i === 0) {
       y = i;
       x = total / y;
-      if ((x - 2) * (y - 2) === yellow) {
+      if ((x - 2) * (y - 2) === yellow) { // 안쪽 가로 * 세로 === yellow가 같아야 한다.
         break;
       }
-    }
   }
 
   return [x, y];
