238de9627f841dac092f99a7e2a6c5e1583e6118
haizel <115685195+haizellatte@users.noreply.github.com>
2023-10-01 00:04:44 +0900

    [level 2] Title: 구명보트, Time: 10.87 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.js
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.js
@@ -1,23 +1,14 @@
 function solution(people, limit) {
-  // people 오름차순 정렬
+  let biggest = 0,
+    count = 0,
+    i = 0;
   people.sort((a, b) => a - b);
-  let count = 0;
-  let [light, heavy] = [0, people.length - 1];
-
-  while (light <= heavy) {
-    if (light === heavy) {
-      count++; 
-      break; //light와 heavy가 같아지면 두 포인터가 겹쳐진 순간으로 모든 요소에 대한 점검이 끝났기에 while문을 종료한다.
-    }
-    if (people[light] + people[heavy] <= limit) {
-      count++;
-      light++;
-      heavy--;
-    } else {
-      count++;
-      heavy--;
-    }
+  while (people.length > 0) {
+    biggest = people.pop();
+    i = 0;
+    while (people[i] <= limit - biggest) i++;
+    if (i) people.splice(i - 1, 1);
+    count++;
   }
-
   return count;
 }
