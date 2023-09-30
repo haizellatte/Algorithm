fa8f88ecd942e906eab5f97033b8f438ea44b462
haizel <115685195+haizellatte@users.noreply.github.com>
2023-10-01 00:31:48 +0900

    [unrated] Title: 귤 고르기, Time: 100.40 ms, Memory: 63.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.js
@@ -0,0 +1,20 @@
+function solution(k, tangerine) {
+  let needTangerine = 0;
+  let result = 0;
+  const typeList = tangerine.reduce((acc, cur) => {
+    acc[cur] = (acc[cur] || 0) + 1;
+    return acc;
+  }, {});
+  const typeKey = Object.entries(typeList)
+    .sort(([a1, b1], [a2, b2]) => b2 - b1) // 내림차순 정렬
+    .map((x) => x[1]); //귤type의 갯수만 뽑아온다.
+
+  for (let key of typeKey) {
+    if (needTangerine >= k) break;
+    else {
+      needTangerine += key;
+      result++;
+    }
+  }
+  return result;
+}
