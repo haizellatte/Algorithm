772dd41fbc6f73448770ba78bb39f671e24eed70
HyoEun Sim <115685195+haizellatte@users.noreply.github.com>
2023-12-18 22:44:30 +0900

    [level 2] Title: 올바른 괄호, Time: 6.38 ms, Memory: 38.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,17 @@
+const solution = (s) => {
+  let stack = [];
+
+  for (let x of s) {
+    if (x === "(") {
+      stack.push(x);
+    } else {
+      if (stack.length === 0) {
+        // 왼쪽, 오른쪽 괄호의 수가 맞지 않아도 false
+        return false;
+      }
+      stack.pop();
+    }
+  }
+  // 모든 원소를 다 돌고 stack에 아무것도 남아있지 않으면 모든 괄호에 대해 짝이 맞다는 것!
+  return stack.length === 0;
+};
